11b5d0aa2e927beff7690714c5b91eda6c0edd54
Mrudul Ladhwe <ladhwe.m@northeastern.edu>
2021-11-06 16:59:34 -0400

    comit

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd /home/ubuntu/webapp/webApp
+sudo apt install python3-pip
+sudo pip install -r requirement.txt
+sudo apt-get install -y python3-psycopg2
 sudo python3 manage.py migrate
 sudo python3 manage.py runserver 0.0.0.0:8001
