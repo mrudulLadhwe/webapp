4fb012ceac17ddfc13905989b444c22396ea9b77
Mrudul Ladhwe <ladhwe.m@northeastern.edu>
2021-12-02 11:47:07 -0500

    d1

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -10,6 +10,7 @@ sudo pip install -r requirement.txt
 sudo apt-get install -y python3-psycopg2
 pip freeze
 ls
+sudo chmod 777 django.log
 sudo python3 manage.py migrate
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/webApp/amazon-cloudwatch-agent.json -s
 sudo nohup python3 manage.py runserver 0.0.0.0:8001 > /dev/null 2>&1 &
